68707abfc09d78653065fc8237112ef9a6a49d27
romeroraven <romeroravenr@gmail.com>
2021-02-14 22:58:59 -0700

    commit tree 9bda797bbd3b187c631e01214c49ecf62a7de436

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+    validates :title, length: { minimum: 2 }
+    validates :content, length: { minimum: 200 }
+    validates :summary, length: { maximum: 100 }
+    validates :category, inclusion: { in:["Fiction", "Non-fiction"] }
+    validate :clickbait
+
+    def clickbait
+        if self.title
+            if self.title[/Won't Believe|Secret|Top|Guess/] == nil
+            errors.add(:clickbait, "post not clickbait")
+            end
+        end
+    end
 end
+
+ 
